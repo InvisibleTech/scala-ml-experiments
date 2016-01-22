8e0ef63ff975dfe852162cbf93b01eba7d575d82
John Ferguson <jferguson@coldlight.com>
2016-01-22 15:34:40 -0500

    Experiment on ML Linear Regression. Need other data.

--- src/test/scala/LinearRegressionSpec.scala
+++ src/test/scala/LinearRegressionSpec.scala
@@ -10,6 +10,11 @@ class LinearRegressionSpec extends FunSpec {
         val l = List(Point(-3, -15), Point(1, 1), Point(5, 17), Point(8, 29), Point(12, 45))
         assert((4.0, -3.0) == LinearRegression.optimizedFit(l))
     }
+
+    // Consider writing a test around test.csv 
+    // asserting    intercept == -0.0107093;
+    // asserting    m == 0.5514;
+
   }
 }
 
